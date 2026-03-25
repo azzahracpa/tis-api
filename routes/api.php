4730fa5b46f71d40b550832a11a8dcea65a83a5a
azzahracpa <zahraracalista13@gmail.com>
2026-03-25 15:00:53 +0700

    tis-api2

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
 
-Route::get('/filterByParam/{author}/{year}', [BookController::class, 'filterBooksByParam'])
-    ->middleware('validate.year');
+Route::prefix('posts')->group(function () {
+    Route::post('/', [PostController::class, 'createPost']);
+    Route::get('/{id}', [PostController::class, 'getPostById']);
+    Route::put('/{id}/tag/{tagId}', [PostController::class, 'addTag']);
+});
 
-Route::get('/filterByQuery', [BookController::class, 'filterBooksByQuery']);
+Route::prefix('comments')->group(function () {
+    Route::post('/', [CommentController::class, 'createComment']);
+});
 
-Route::prefix('books')->group(function () {
-    Route::get('/', [BookController::class, 'getAll'])->name('books.all');
-    Route::get('/{id}', [BookController::class, 'getOne']);
-    Route::post('/', [BookController::class, 'create']);
-    Route::put('/{id}', [BookController::class, 'update']);
-    Route::delete('/{id}', [BookController::class, 'delete']);
+Route::prefix('tags')->group(function () {
+    Route::post('/', [TagController::class, 'createTag']);
 });
