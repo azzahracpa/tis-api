6995e78f53360bb243bc2db81439f443f5eb1203
azzahracpa <zahraracalista13@gmail.com>
2026-04-27 23:25:58 +0700

    Menambahkan intgerasi Public API JSONPlaceholder

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\PublicPostController; // ← tambahan
 
 Route::prefix('posts')->group(function () {
     Route::post('/', [PostController::class, 'createPost']);
@@ -18,3 +20,13 @@ Route::prefix('comments')->group(function () {
 Route::prefix('tags')->group(function () {
     Route::post('/', [TagController::class, 'createTag']);
 });
+
+Route::prefix('books')->group(function () {
+    Route::get('/', [BookController::class, 'getAll']);
+    Route::get('/{id}', [BookController::class, 'getOne']);
+    Route::post('/', [BookController::class, 'create']);
+    Route::put('/{id}', [BookController::class, 'update']);
+    Route::delete('/{id}', [BookController::class, 'delete']);
+});
+
+Route::get('/public-posts', [PublicPostController::class, 'getAllPosts']);
